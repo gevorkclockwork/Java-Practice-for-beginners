1f6187ada995281a127c722bcb2d14b6ebd36020
gevorkclockwork <geloryan@gmail.com>
2024-10-13 19:05:28 +0300

    add day 8

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,56 @@
+package day8;
+
+public class Airplane {
+    public String manufacturer;
+    public int year;
+    public int length;
+    public int weight;
+    public int fuel;
+
+    public Airplane(String manufacturer, int year, int length, int weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel);
+    }
+
+    public String toString() {
+        return "Изготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel;
+    }
+
+
+    public void fillUp(int n) {
+        this.fuel += n;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,23 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long startTime1 = System.currentTimeMillis();
+        String numbers = "";
+        for (int i = 0; i < 20001; i++) {
+            numbers += i + " ";
+        }
+        System.out.println(numbers);
+        long stopTime1 = System.currentTimeMillis();
+
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 20001; i++) {
+            stringBuilder.append(i + " ");
+        }
+        System.out.println(stringBuilder);
+        long stopTime2 = System.currentTimeMillis();
+
+        System.out.println("Длительность работы, в мс.: " + (stopTime1 - startTime1));
+        System.out.println("Длительность работы, в мс.: " + (stopTime2 - startTime2));
     }
 }
