45d7985da5313beb0f19f0a442cd85af86a59b45
gevorkclockwork <geloryan@gmail.com>
2024-10-23 08:24:19 +0300

    add day 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,32 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+
+        File file = new File("src/main/resources/shoes.csv");
+        File fileOut = new File("missing_shoes.txt");
+
+        try {
+            Scanner scanner = new Scanner(file);
+            PrintWriter pw = new PrintWriter(fileOut);
+            while (scanner.hasNextLine()) {
+                String[] array = scanner.nextLine().split(";");
+                if (Integer.parseInt(array[2]) == 0) {
+                    pw.println(array[0] + ", " + array[1] + ", " + array[2]);
+                }
+            }
+            pw.close();
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
 
     }
 }
