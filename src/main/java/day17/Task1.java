24aeb1b62af7f1aecf3b299c167ebe4d6e05f865
gevorkclockwork <geloryan@gmail.com>
2024-10-24 17:24:04 +0300

    add day 17

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,29 @@
 package day17;
 
+import java.util.Arrays;
+
+import static day17.ChessPiece.PAWN_WHITE;
+import static day17.ChessPiece.ROOK_BLACK;
+
 public class Task1 {
     public static void main(String[] args) {
+
+        ChessPiece[] chessPieces = {PAWN_WHITE,PAWN_WHITE,PAWN_WHITE,PAWN_WHITE, ROOK_BLACK,ROOK_BLACK,ROOK_BLACK,ROOK_BLACK};
+
+//        for (int i = 0; i < 4; i++) {
+//            chessPieces[i] = PAWN_WHITE;
+//        }
+//
+//        for (int i = 4; i < 8; i++) {
+//            chessPieces[i] = ROOK_BLACK;
+//        }
+
+        for (ChessPiece chessPiece : chessPieces) {
+            System.out.print(chessPiece.getFigureDesignation() + " ");
+        }
+
+
+
 
     }
 }
